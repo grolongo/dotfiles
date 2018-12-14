514aa2f98dff124de717702fe186220eedd9c15a
grolongo <34292770+grolongo@users.noreply.github.com>
2018-12-14 16:18:32 +0100

    homebrew install test and exports variables

--- install/install-macos.sh
+++ install/install-macos.sh
@@ -171,7 +171,7 @@ setup_network() {
 install_homebrew() {
   check_is_not_sudo
 
-  if test ! "$(which brew)"
+  if test ! "$(command -v brew >/dev/null 2>&1)"
   then
     msg_info "Downloading and installing Homebrew..."
     /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
