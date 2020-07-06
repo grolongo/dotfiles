316a361a497460d3a31c5591d40148fd11bd3cf6
grolongo <34292770+grolongo@users.noreply.github.com>
2020-07-06 12:27:19 +0200

    indent and 7zip remove

--- install/symlinks-windows.ps1
+++ install/symlinks-windows.ps1
@@ -20,41 +20,41 @@ if ($confirmation -eq 'y') {
 # mpv
 $confirmation = Read-Host "Symlink mpv?"
 if ($confirmation -eq 'y') {
-  New-Item -Force -Path "$env:APPDATA\mpv" -ItemType directory
-  New-Item -Force -Path "$env:APPDATA\mpv\mpv.conf" -ItemType SymbolicLink -Value "$base\.config\mpv\mpv.conf"
-  New-Item -Force -Path "$env:APPDATA\mpv\input.conf" -ItemType SymbolicLink -Value "$base\.config\mpv\input.conf"
+    New-Item -Force -Path "$env:APPDATA\mpv" -ItemType directory
+    New-Item -Force -Path "$env:APPDATA\mpv\mpv.conf" -ItemType SymbolicLink -Value "$base\.config\mpv\mpv.conf"
+    New-Item -Force -Path "$env:APPDATA\mpv\input.conf" -ItemType SymbolicLink -Value "$base\.config\mpv\input.conf"
 }
 
 # streamlink
 $confirmation = Read-Host "Symlink streamlink?"
 if ($confirmation -eq 'y') {
-  New-Item -Force -Path "$env:APPDATA\streamlink" -ItemType directory
-  New-Item -Force -Path "$env:APPDATA\streamlink\streamlinkrc" -ItemType SymbolicLink -Value "$base\.streamlinkrc"
+    New-Item -Force -Path "$env:APPDATA\streamlink" -ItemType directory
+    New-Item -Force -Path "$env:APPDATA\streamlink\streamlinkrc" -ItemType SymbolicLink -Value "$base\.streamlinkrc"
 }
 
 # markdownlinter
 $confirmation = Read-Host "Symlink mdlrc?"
 if ($confirmation -eq 'y') {
-  New-Item -Force -Path "$HOME\.mdlrc" -ItemType SymbolicLink -Value "$base\.mdlrc"
+    New-Item -Force -Path "$HOME\.mdlrc" -ItemType SymbolicLink -Value "$base\.mdlrc"
 }
 
 # wsltty
 $confirmation = Read-Host "Symlink wsltty?"
 if ($confirmation -eq 'y') {
-  New-Item -Force -Path "$env:APPDATA\wsltty\config" -ItemType SymbolicLink -Value "$base\wsltty\config"
-  New-Item -Force -Path "$env:APPDATA\wsltty\themes\solarized_dark" -ItemType SymbolicLink -Value "$base\wsltty\themes\solarized_dark"
+    New-Item -Force -Path "$env:APPDATA\wsltty\config" -ItemType SymbolicLink -Value "$base\wsltty\config"
+    New-Item -Force -Path "$env:APPDATA\wsltty\themes\solarized_dark" -ItemType SymbolicLink -Value "$base\wsltty\themes\solarized_dark"
 }
 
 # git
 $confirmation = Read-Host "Symlink gitconfig?"
 if ($confirmation -eq 'y') {
-  New-Item -Force -Path "$HOME\.gitconfig" -ItemType SymbolicLink -Value "$base\.gitconfig"
+    New-Item -Force -Path "$HOME\.gitconfig" -ItemType SymbolicLink -Value "$base\.gitconfig"
 }
 
 # curl
 $confirmation = Read-Host "Symlink curlrc?"
 if ($confirmation -eq 'y') {
-  New-Item -Force -Path "$HOME\_curlrc" -ItemType SymbolicLink -Value "$base\.curlrc"
+    New-Item -Force -Path "$HOME\_curlrc" -ItemType SymbolicLink -Value "$base\.curlrc"
 }
 
 # aria2
