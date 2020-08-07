d26bc97daf0abf8f51ef764f821ca326cb8d09e8
grolongo <34292770+grolongo@users.noreply.github.com>
2020-08-07 22:45:44 +0200

    dont remove xbox app

--- install/install-windows.ps1
+++ install/install-windows.ps1
@@ -214,7 +214,6 @@ function remove_junk {
     Get-AppxPackage Microsoft.WindowsFeedbackHub | Remove-AppxPackage
     Get-AppxPackage Microsoft.WindowsMaps | Remove-AppxPackage
     Get-AppxPackage Microsoft.WindowsPhone | Remove-AppxPackage
-    Get-AppxPackage Microsoft.XboxApp | Remove-AppxPackage
     Get-AppxPackage Microsoft.XboxIdentityProvider | Remove-AppxPackage
     Get-AppxPackage Microsoft.XboxSpeechToTextOverlay | Remove-AppxPackage
     Get-AppxPackage Microsoft.ZuneMusic | Remove-AppxPackage
