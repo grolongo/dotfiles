80129f7867ca8b0b7451311cc4b35c437dbe440c
grolongo <34292770+grolongo@users.noreply.github.com>
2021-01-12 19:24:14 +0100

    better alias names

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,8 +1,16 @@
 function My-UpgradeChoco {Start-Process powershell.exe -Verb RunAs 'choco upgrade all'}
 
-function My-Find {
+function Get-Filename {
     $query = $args[0]
     Get-ChildItem -Recurse -Force -ErrorAction SilentlyContinue -Filter *$query* | %{ $_.FullName }
 }
+New-Alias -Name My-Find -Value Get-Filename
 
-function My-Grep {Get-ChildItem -Recurse | Select-String -Pattern $args[0]}
+function Get-String {Get-ChildItem -Recurse | Select-String -Pattern $args[0]}
+New-Alias -Name My-Grep -Value Get-String
+
+function Get-LocalRipgrep {rg --follow --hidden --max-depth 1 --search-zip --smart-case -e $args[0]}
+New-Alias -Name My-LRg -Value Get-LocalRipgrep
+
+function Get-Ripgrep {rg --follow --hidden --search-zip --smart-case $args[0]}
+New-Alias -Name My-Rg -Value Get-Ripgrep
