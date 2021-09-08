4c9ac6fb993e7341702a2bde313c30e33d7e882c
grolongo <34292770+grolongo@users.noreply.github.com>
2021-09-08 19:02:58 +0200

    further alias tweaks

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -18,23 +18,27 @@ New-Alias -Name My-Find2 -Value Get-Filename2
 
 ### using findstr.exe
 function Get-LocalString {findstr /i /p /n $args[0] *}
-New-Alias -Name My-LocalGrep -Value Get-LocalString
+New-Alias -Name lfindstr -Value Get-LocalString
 
 function Get-String {findstr /s /i /p /n $args[0] *}
-New-Alias -Name My-Grep -Value Get-String
+New-Alias -Name rfindstr -Value Get-String
 
 ### using powershell cmdlets
 function Get-LocalString2 {Get-ChildItem | Select-String -Pattern $args[0]}
-New-Alias -Name My-LocalGrep2 -Value Get-LocalString2
+New-Alias -Name lgetchilditem -Value Get-LocalString2
 
 function Get-String2 {Get-ChildItem -Recurse | Select-String -Pattern $args[0]}
-New-Alias -Name My-Grep2 -Value Get-String2
+New-Alias -Name rgetchilditem -Value Get-String2
 
 ### using third-party ripgrep
 function Get-LocalRipgrep {rg --follow --hidden --max-depth 1 --search-zip --smart-case -e $args[0]}
-New-Alias -Name My-LRg -Value Get-LocalRipgrep
+New-Alias -Name lrg -Value Get-LocalRipgrep
 
 function Get-Ripgrep {rg --follow --hidden --search-zip --smart-case $args[0]}
-New-Alias -Name My-Rg -Value Get-Ripgrep
+New-Alias -Name rrg -Value Get-Ripgrep
 
-
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+    Import-Module "$ChocolateyProfile"
+}
