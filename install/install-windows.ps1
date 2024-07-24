70e59de64ca5a97db7804c1a81b08d37d450b348
grolongo <34292770+grolongo@users.noreply.github.com>
2024-07-24 08:52:37 +0200

    add windows install tweaks

--- install/install-windows.ps1
+++ install/install-windows.ps1
@@ -7,7 +7,9 @@
 function set_uipreferences {
     $explorer = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer'
     $exploreradvanced = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced'
-    $searchcortana = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search'
+    $search = 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search'
+    $explorerpolicies ='HKCU:\\Software\\Policies\\Microsoft\\Windows\\Explorer'
+    $theme ='HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize'
 
     if(Test-Path -Path $explorer) {
         # show icons notification area (always show = 0, not showing = 1)
@@ -32,15 +34,43 @@ function set_uipreferences {
         Write-Host -ForegroundColor "yellow" "Setting taskbar combine when full mode..."
         Set-ItemProperty -Path $exploreradvanced -Name 'TaskbarGlomLevel' -Value 1
 
+        # Win11 icons to the left (center = 1, left = 0)
+        Write-Host -ForegroundColor "yellow" "Icons to the left..."
+        Set-ItemProperty -Path $exploreradvanced -Name "TaskbarAl" -Value 0
+
         # lock taskbar (lock = 0, unlock = 1)
         Write-Host -ForegroundColor "yellow" "Locking the taskbar..."
         Set-ItemProperty -Path $exploreradvanced -Name 'TaskbarSizeMove' -Value 0
+
+        # disable widgets (on = 1, off = 0)
+        Write-Host -ForegroundColor "yellow" "Disabling Taskbar Widgets..."
+        Set-ItemProperty -Path $exploreradvanced -Name 'TaskbarDa' -Value 0
+
+        # disable snap assist (on = 1, off = 0)
+        Write-Host -ForegroundColor "yellow" "Disabling snap assist..."
+        taskkill.exe /F /IM "explorer.exe"
+        Set-ItemProperty -Path $exploreradvanced -Name SnapAssist -Value 0
+        Start-Process "explorer.exe"
     }
 
-    if(Test-Path -Path $searchcortana) {
+    if(Test-Path -Path $search) {
         # search box (bar = 2, icon = 1, nothing = 0)
         Write-Host -ForegroundColor "yellow" "Removing Cortana search bar..."
-        Set-ItemProperty -Path $searchcortana -Name 'SearchboxTaskbarMode' -Value 0
+        Set-ItemProperty -Path $search -Name 'SearchboxTaskbarMode' -Value 0
+    }
+
+    if(!(Test-Path -Path $explorerpolicies)) {
+        Write-Host -ForegroundColor "yellow" "Disabling web suggestions in search..."
+        New-Item -Path 'HKCU:\\SOFTWARE\\Policies\\Microsoft\\Windows\\Explorer' -Force | Out-Null
+        New-ItemProperty -Path 'HKCU:\\SOFTWARE\\Policies\\Microsoft\\Windows\\Explorer' -Name 'DisableSearchBoxSuggestions' -Type DWord -Value 1 -Force
+        Stop-Process -name explorer -force
+    }
+
+    if(Test-Path -Path $theme) {
+        # dark theme (dark theme = 0, light theme = 1)
+        Write-Host -ForegroundColor "yellow" "Enabling dark mode..."
+        Set-ItemProperty -Path $theme -Name AppsUseLightTheme -Value 0
+        Set-ItemProperty -Path $theme -Name SystemUsesLightTheme -Value 0
     }
 
     Write-Host -ForegroundColor "yellow" "Disabling sounds..."
@@ -289,6 +319,102 @@ function install_qbittorrent {
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/CravateRouge/qBittorrentSearchPlugins/master/yggtorrent.py" -OutFile "$PLUGIN_FOLDER\yggtorrent.py"
 }
 
+### OneDrive
+
+function remove_onedrive {
+
+    # taken from Chris Titus script
+
+    $OneDrivePath = $($env:OneDrive)
+    Write-Host \"Removing OneDrive\"
+    $regPath = \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Uninstall\\OneDriveSetup.exe\"
+
+    if (Test-Path $regPath){
+        $OneDriveUninstallString = Get-ItemPropertyValue \"$regPath\" -Name \"UninstallString\"
+        $OneDriveExe, $OneDriveArgs = $OneDriveUninstallString.Split(\" \")
+        Start-Process -FilePath $OneDriveExe -ArgumentList \"$OneDriveArgs /silent\" -NoNewWindow -Wait
+    }
+    else{
+        Write-Host \"Onedrive dosn't seem to be installed anymore\" -ForegroundColor Red
+        return
+    }
+
+    # Check if OneDrive got Uninstalled
+    if (-not (Test-Path $regPath)){
+        Write-Host \"Copy downloaded Files from the OneDrive Folder to Root UserProfile\"
+        Start-Process -FilePath powershell -ArgumentList \"robocopy '$($OneDrivePath)' '$($env:USERPROFILE.TrimEnd())\\' /mov /e /xj\" -NoNewWindow -Wait
+
+        Write-Host \"Removing OneDrive leftovers\"
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:localappdata\\Microsoft\\OneDrive\"
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:localappdata\\OneDrive\"
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:programdata\\Microsoft OneDrive\"
+        Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$env:systemdrive\\OneDriveTemp\"
+        reg delete \"HKEY_CURRENT_USER\\Software\\Microsoft\\OneDrive\" -f
+        # check if directory is empty before removing:
+        If ((Get-ChildItem \"$OneDrivePath\" -Recurse | Measure-Object).Count -eq 0) {
+            Remove-Item -Recurse -Force -ErrorAction SilentlyContinue \"$OneDrivePath\"
+        }
+
+        Write-Host \"Remove Onedrive from explorer sidebar\"
+        Set-ItemProperty -Path \"HKCR:\\CLSID\\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\" -Name \"System.IsPinnedToNameSpaceTree\" -Value 0
+        Set-ItemProperty -Path \"HKCR:\\Wow6432Node\\CLSID\\{018D5C66-4533-4307-9B53-224DE2ED1FE6}\" -Name \"System.IsPinnedToNameSpaceTree\" -Value 0
+
+        Write-Host \"Removing run hook for new users\"
+        reg load \"hku\\Default\" \"C:\\Users\\Default\\NTUSER.DAT\"
+        reg delete \"HKEY_USERS\\Default\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run\" /v \"OneDriveSetup\" /f
+        reg unload \"hku\\Default\"
+
+        Write-Host \"Removing startmenu entry\"
+        Remove-Item -Force -ErrorAction SilentlyContinue \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\OneDrive.lnk\"
+
+        Write-Host \"Removing scheduled task\"
+        Get-ScheduledTask -TaskPath '\\' -TaskName 'OneDrive*' -ea SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
+
+        # Add Shell folders restoring default locations
+        Write-Host \"Shell Fixing\"
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"AppData\" -Value \"$env:userprofile\\AppData\\Roaming\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Cache\" -Value \"$env:userprofile\\AppData\\Local\\Microsoft\\Windows\\INetCache\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Cookies\" -Value \"$env:userprofile\\AppData\\Local\\Microsoft\\Windows\\INetCookies\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Favorites\" -Value \"$env:userprofile\\Favorites\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"History\" -Value \"$env:userprofile\\AppData\\Local\\Microsoft\\Windows\\History\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Local AppData\" -Value \"$env:userprofile\\AppData\\Local\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"My Music\" -Value \"$env:userprofile\\Music\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"My Video\" -Value \"$env:userprofile\\Videos\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"NetHood\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Network Shortcuts\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"PrintHood\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Printer Shortcuts\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Programs\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Recent\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Recent\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"SendTo\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\SendTo\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Start Menu\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Startup\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Start Menu\\Programs\\Startup\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Templates\" -Value \"$env:userprofile\\AppData\\Roaming\\Microsoft\\Windows\\Templates\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"{374DE290-123F-4565-9164-39C4925E467B}\" -Value \"$env:userprofile\\Downloads\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Desktop\" -Value \"$env:userprofile\\Desktop\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"My Pictures\" -Value \"$env:userprofile\\Pictures\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"Personal\" -Value \"$env:userprofile\\Documents\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"{F42EE2D3-909F-4907-8871-4C22FC0BF756}\" -Value \"$env:userprofile\\Documents\" -Type ExpandString
+        Set-ItemProperty -Path \"HKCU:\\Software\\Microsoft\\Windows\\CurrentVersion\\Explorer\\User Shell Folders\" -Name \"{0DDD015D-B06C-45D5-8C4C-F59713854639}\" -Value \"$env:userprofile\\Pictures\" -Type ExpandString
+        Write-Host \"Restarting explorer\"
+        taskkill.exe /F /IM \"explorer.exe\"
+        Start-Process \"explorer.exe\"
+
+        Write-Host \"Waiting for explorer to complete loading\"
+        Write-Host \"Please Note - The OneDrive folder at $OneDrivePath may still have items in it. You must manually delete it, but all the files should already be copied to the base user folder.\"
+        Write-Host \"If there are Files missing afterwards, please Login to Onedrive.com and Download them manually\" -ForegroundColor Yellow
+        Start-Sleep 5
+    }
+    else{
+        Write-Host \"Something went Wrong during the Unistallation of OneDrive\" -ForegroundColor Red
+    }
+}
+
+### Copilot
+
+function remove_copilot {
+    Write-Host -ForegroundColor "yellow" "Removing Copilot..."
+    dism /online /remove-package /package-name:Microsoft.Windows.Copilot
+}
+
 ### Dotfiles
 
 function set_dotfiles {
@@ -309,13 +435,15 @@ function usage {
     Write-Host "  powersettings     - disables power saving modes on AC power"
     Write-Host "  envar             - setups environment variables"
     Write-Host "  hostname          - changes hostname"
-    Write-Host "  features          - enables several Windows Optional Features"
+    Write-Host "  features          - enables Hyper-V and Disposable Desktop"
     Write-Host "  ctrltocaps        - remap CTRL key to Caps Lock"
     Write-Host "  wsl               - enables WSL2 and installs Ubuntu or Debian"
     Write-Host "  chocolatey        - downloads and sets chocolatey package manager"
     Write-Host "  choco_packages    - downloads and installs listed packages with chocolatey"
     Write-Host "  winget_packages   - downloads and installs listed packages with winget"
     Write-Host "  qbit              - installs qBittorrent with plugins"
+    Write-Host "  onedrive          - removes OneDrive entirely"
+    Write-Host "  copilot           - removes Copilot entirely"
     Write-Host "  dotfiles          - launches external dotfiles script"
 Write-Host
 }
@@ -340,6 +468,8 @@ function main {
     elseif ($cmd -eq "choco_packages") { install_choco }
     elseif ($cmd -eq "winget_packages") { install_winget }
     elseif ($cmd -eq "qbit") { install_qbittorrent }
+    elseif ($cmd -eq "onedrive") { remove_onedrive }
+    elseif ($cmd -eq "copilot") { remove_copilot }
     elseif ($cmd -eq "dotfiles") { set_dotfiles }
     else { usage }
 }
